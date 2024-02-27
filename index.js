34c4271950c7e8dd842231bc4503fecc04d77f05
Alina-Araujo <alina_araujo@hotmail.com>
2024-02-27 14:27:41 -0300

    segundo commit

---
+++ index.js
@@ -0,0 +1,29 @@
+let heroi = "Oli"
+let xp = 8500
+    if (xp) {
+        case <= 1000 ;
+            console.log ("Ferro")
+            break
+            case <= 2000 ;
+                console.log ("Bronze")
+                break
+                case <= 5000 ;
+                    console.log ("Prata")
+                    break
+                    case <= 7000 ;
+                        console.log ("Ouro")
+                        break
+                        case <= 8000 ;
+                            console.log ("Platina")
+                            break
+                            case <= 9000 ;
+                                console.log ("Ascendente")
+                                break
+                                case <= 10000 ;
+                                    console.log ("Imortal")
+                                    break
+                                    default 
+                                    console.log ("Radiante")
+ }
+ console.log ("O" + heroi "está no nível" + xp)
+ 
